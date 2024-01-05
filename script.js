3a373f6507de78d7bc343881753e271c68f8ea89
RafaelZucarelli <zucarelli83@gmail.com>
2024-01-05 15:01:21 -0300

    progress

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function contar(){
+    let fnum = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+    
+    if (fnum.value.length == 0) {
+    window.alert ('[ERRO] Por favor digite um número')
+    
+    } else {
+    let n = Number(fnum.value)
+    let cont = 1
+    tab.innerHTML = ''
+    while (cont <=10 ){
+        let item = document.createElement('option')
+        item.text = `${n} X ${cont} = ${n*cont}`
+        item.value = `tab${cont}`
+        tab.appendChild(item)
+        cont ++
+    }
+    }
+}
